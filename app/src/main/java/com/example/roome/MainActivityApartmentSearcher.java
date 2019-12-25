11e0c40e3e83b1a473a9a59df6063ca5b2bb431b
LevYuval <51016580+LevYuval@users.noreply.github.com>
2019-12-25 12:10:00 +0200

    add icons

--- app/src/main/java/com/example/roome/MainActivityApartmentSearcher.java
+++ app/src/main/java/com/example/roome/MainActivityApartmentSearcher.java
@@ -26,19 +26,18 @@ public class MainActivityApartmentSearcher extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private CustomViewPager viewPager;
-    private int[] selectedtabIcons = {
-            R.drawable.ic_action_filled_heart,
+    private int[] selectedtabIcons = {R.drawable.ic_action_filled_home,
+            R.drawable.ic_action_filled_heart, R.drawable.ic_action_filled_hourglass,
             R.drawable.ic_action_filled_person};
 
-    private int[] unselectedtabIcons = {
-            R.drawable.ic_action_empty_heart,
+    private int[] unselectedtabIcons = { R.drawable.ic_action_empty_home,
+            R.drawable.ic_action_empty_heart,R.drawable.ic_action_empty_hourglass,
             R.drawable.ic_action_empty_person};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_apartment_searcher);
-
 
 
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -55,14 +54,15 @@ public class MainActivityApartmentSearcher extends AppCompatActivity {
                     @Override
                     public void onTabSelected(TabLayout.Tab tab) {
                         super.onTabSelected(tab);
-                     tab.setIcon(selectedtabIcons[tab.getPosition()]);}
+                        tab.setIcon(selectedtabIcons[tab.getPosition()]);
+                    }
 
                     @Override
                     public void onTabUnselected(TabLayout.Tab tab) {
                         super.onTabUnselected(tab);
 
-                            tab.setIcon(unselectedtabIcons[tab.getPosition()]);
-                        }
+                        tab.setIcon(unselectedtabIcons[tab.getPosition()]);
+                    }
 
                     @Override
                     public void onTabReselected(TabLayout.Tab tab) {
@@ -74,19 +74,25 @@ public class MainActivityApartmentSearcher extends AppCompatActivity {
 //        androidx.appcompat.widget.Toolbar apartment_toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.apartment_tool_bar);
 //        setSupportActionBar(apartment_toolbar);
     }
+
     private void setupTabIcons() {
         tabLayout.getTabAt(0).setIcon(selectedtabIcons[0]);
         tabLayout.getTabAt(1).setIcon(unselectedtabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(unselectedtabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(unselectedtabIcons[3]);
     }
 
 
     private void setupViewPager(ViewPager viewPager) {
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "ONE");
-        adapter.addFragment(new TwoFragment(), "TWO");
+        adapter.addFragment(new OneFragment(), "HOME");
+        adapter.addFragment(new TwoFragment(), "MATCHES");
+        adapter.addFragment(new ThreeFragment(), "FILTERS");
+        adapter.addFragment(new FourFragment(), "PROFILE");
         viewPager.setAdapter(adapter);
     }
+
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
         private final List<String> mFragmentTitleList = new ArrayList<>();
@@ -99,6 +105,7 @@ public class MainActivityApartmentSearcher extends AppCompatActivity {
         public Fragment getItem(int position) {
             return mFragmentList.get(position);
         }
+
         @Override
         public int getCount() {
             return mFragmentList.size();
