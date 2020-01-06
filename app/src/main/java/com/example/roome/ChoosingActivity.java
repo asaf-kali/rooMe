79474e10b409296ddaeaed91c27bf6c80c7d1df9
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-01-06 18:29:07 +0200

    add user data to firebase

--- app/src/main/java/com/example/roome/ChoosingActivity.java
+++ app/src/main/java/com/example/roome/ChoosingActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.roome.user_classes.User;
@@ -13,8 +14,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ChoosingActivity extends AppCompatActivity {
 
@@ -23,6 +29,9 @@ public class ChoosingActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mFirebaseDatabaseReference;
+    final ArrayList<String>[] allApartmentSearcherIds = new ArrayList[1];
+    final ArrayList<String>[] allRoommateSearcherIds = new ArrayList[1];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +43,31 @@ public class ChoosingActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
         mFirebaseDatabaseReference = mFirebaseDatabase.getReference();
+
+        mFirebaseDatabaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                allApartmentSearcherIds[0] = FirebaseMediate.getAllApartmentSearcherIds(dataSnapshot);
+                allRoommateSearcherIds[0] = FirebaseMediate.getAllRoommateSearcherIds(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     /**
      * The function displays the user's name (from which it got from the login) in this
      * activity
      */
-    private void updateUserName() {
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(ChoosingActivity.this);
+    private void updateUserName() {//todo
         String userName = "";
-        if (acct != null) {
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(ChoosingActivity.this);
+        if (mFirebaseUser != null) {
+            userName = mFirebaseUser.getDisplayName();
+        } else if (acct != null) {
             userName = acct.getDisplayName();
         }
         TextView textView = findViewById(R.id.tv_hello_name);
@@ -64,6 +88,11 @@ public class ChoosingActivity extends AppCompatActivity {
     public void roommateSearcherOnclick(View view) {
         User userObj = createNewUser();
         mFirebaseDatabaseReference.child("users").child("RoommateSearcherUser").child(mFirebaseUser.getUid()).setValue(userObj);
+
+        mFirebaseDatabaseReference.child("preferences").child("RoommateSearcherUser").child(mFirebaseUser.getUid()).child("0").setValue(allApartmentSearcherIds[0]);
+        mFirebaseDatabaseReference.child("preferences").child("RoommateSearcherUser").child(mFirebaseUser.getUid()).child("1").setValue(null);
+        mFirebaseDatabaseReference.child("preferences").child("RoommateSearcherUser").child(mFirebaseUser.getUid()).child("2").setValue(null);
+        mFirebaseDatabaseReference.child("preferences").child("RoommateSearcherUser").child(mFirebaseUser.getUid()).child("3").setValue(null);
         toastMessage("Adding " + mFirebaseUser.getDisplayName() + " to database..."); //todo remove
         Intent i = new Intent(ChoosingActivity.this, MainActivityRoommateSearcher.class);
         startActivity(i);
@@ -73,6 +102,11 @@ public class ChoosingActivity extends AppCompatActivity {
     public void apartmentSearcherOnclick(View view) {
         User userObj = createNewUser();
         mFirebaseDatabaseReference.child("users").child("ApartmentSearcherUser").child(mFirebaseUser.getUid()).setValue(userObj);
+
+        mFirebaseDatabaseReference.child("preferences").child("ApartmentSearcherUser").child(mFirebaseUser.getUid()).child("0").setValue(allRoommateSearcherIds);
+        mFirebaseDatabaseReference.child("preferences").child("ApartmentSearcherUser").child(mFirebaseUser.getUid()).child("1").setValue(null);
+        mFirebaseDatabaseReference.child("preferences").child("ApartmentSearcherUser").child(mFirebaseUser.getUid()).child("2").setValue(null);
+        mFirebaseDatabaseReference.child("preferences").child("ApartmentSearcherUser").child(mFirebaseUser.getUid()).child("3").setValue(null);
         toastMessage("Adding " + mFirebaseUser.getDisplayName() + " to database..."); //todo remove
         Intent i = new Intent(ChoosingActivity.this, MainActivityApartmentSearcher.class);
         startActivity(i);
