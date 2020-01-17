ac9c318270081ff96883aef198988233228a967b
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-01-17 13:39:27 +0200

    add ApartmentSearcherOnBoard dialog activity.

---
+++ app/src/main/java/com/example/roome/ApartmentSearcherOnBoardDialogActivity.java
@@ -0,0 +1,23 @@
+package com.example.roome;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ApartmentSearcherOnBoardDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apartment_searcher_on_board);
+    }
+
+    /**
+     * Closes the window when clicked ok or outside the window
+     * @param okButton - The ok button in the dialog frame.
+     */
+    public void onClickOk(View okButton){
+        finish();
+    }
+}
