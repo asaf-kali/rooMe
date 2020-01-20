a291b793932e1eb6872970a6dab7363eb13aefdf
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-01-20 15:21:13 +0200

    fix main activity flow and remove irrelevant firebase code

--- app/src/main/java/com/example/roome/MainActivity.java
+++ app/src/main/java/com/example/roome/MainActivity.java
@@ -10,11 +10,6 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 /**
  * this activity is presenting the app logo for a few seconds and then disappears.
  * this activity will be shown on every app visit.
@@ -26,32 +21,17 @@ public class MainActivity extends AppCompatActivity {
     //Time passed till next activity is launched
     private static final int TIME_OUT = 3000;
 
-    private String userName;
-    private String mPhotoUrl;
-
-    // Firebase instance variables
-    private FirebaseAuth firebaseAuth;
-    private FirebaseUser firebaseUser;
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference firebaseDatabaseReference;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Initialize Firebase
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseUser = firebaseAuth.getCurrentUser();
-        firebaseDatabaseReference = firebaseDatabase.getReference();
         FirebaseMediate.setDataSnapshot();
         Data.initD();
         //todo delete 4 rows
-//        final SharedPreferences reader = getApplicationContext().getSharedPreferences(MyPreferences.MY_PREFERENCES, Context.MODE_PRIVATE);
-//        final SharedPreferences.Editor editor = reader.edit();
-//        editor.putBoolean(MyPreferences.IS_FIRST_TIME, true);
-//        editor.apply();
+        final SharedPreferences reader = getApplicationContext().getSharedPreferences(MyPreferences.MY_PREFERENCES, Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = reader.edit();
+        editor.putBoolean(MyPreferences.IS_FIRST_TIME, true);
+        editor.apply();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
