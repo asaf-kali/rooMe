3f07b449a5cf4522354c082b6e1d7d9566a9b7f0
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-03-01 15:34:38 +0200

    clean code - user classes

--- app/src/main/java/com/example/roome/user_classes/RoommateSearcherUser.java
+++ app/src/main/java/com/example/roome/user_classes/RoommateSearcherUser.java
@@ -4,43 +4,70 @@ import android.media.Image;
 
 import java.util.ArrayList;
 
+/**
+ * A class representing a RoommateSearcherUser - a user who's looking for roommates for his apartment.
+ */
 public class RoommateSearcherUser extends User {
 
-    //--------------------profile additionalInfo---------------------
-    private ArrayList<Image> roommatesImages; //todo: maybe delete
+    //--------------------profile additionalInfo------------
+    private ArrayList<Image> roommatesImages;
     private String additionalInfo; //such as additionalInfo
     private Apartment apartment;
 
-    //--------------------filters---------------------
+    //--------------------filters---------------------------
     private String genderPreference;
 
-
-    public RoommateSearcherUser(String firstName, String lastName, int age) {
+    /**
+     * a constructor for RoommateSearcherUser.
+     *
+     * @param firstName - users first name.
+     * @param lastName  - users last name.
+     */
+    public RoommateSearcherUser(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
+    /**
+     * a constructor for RoommateSearcherUser.
+     */
     public RoommateSearcherUser() {
         super();
     }
 
     //------------------------------------------Getters---------------------------------------------
 
-
+    /**
+     * geter for the roommatesImages.
+     *
+     * @return a list of the users images.
+     */
     public ArrayList<Image> getRoommatesImages() {
         return roommatesImages;
     }
 
-
+    /**
+     * geter for users additionalInfo.
+     *
+     * @return users additional info.
+     */
     public String getAdditionalInfo() {
         return additionalInfo;
     }
 
-
+    /**
+     * geter for genderPreference.
+     *
+     * @return the gender preference for the roommate.
+     */
     public String getGenderPreference() {
         return genderPreference;
     }
 
-
+    /**
+     * geter for the apartment.
+     *
+     * @return the users the apartment.
+     */
     public Apartment getApartment() {
         return apartment;
     }
@@ -48,21 +75,38 @@ public class RoommateSearcherUser extends User {
 
     //------------------------------------------Setters---------------------------------------------
 
-
+    /**
+     * seter for the roommatesImages.
+     *
+     * @param roommatesImages users list of the images.
+     */
     public void setRoommatesImages(ArrayList<Image> roommatesImages) {
         this.roommatesImages = roommatesImages;
     }
 
-
+    /**
+     * seter for users additionalInfo.
+     *
+     * @param info - users additional info.
+     */
     public void setAdditionalInfo(String info) {
         this.additionalInfo = info;
     }
 
-
+    /**
+     * seter for users gender preference.
+     *
+     * @param genderPreference - users gender preference.
+     */
     public void setGenderPreference(String genderPreference) {
         this.genderPreference = genderPreference;
     }
 
+    /**
+     * seter for users apartment.
+     *
+     * @param apartment users apartment.
+     */
     public void setApartment(Apartment apartment) {
         this.apartment = apartment;
     }
