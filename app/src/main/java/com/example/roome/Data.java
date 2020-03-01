45b6cf83f266d6ba714047bee7a99008f2572cfe
LevYuval <51016580+LevYuval@users.noreply.github.com>
2020-03-01 12:59:46 +0200

    fix

--- app/src/main/java/com/example/roome/Data.java
+++ app/src/main/java/com/example/roome/Data.java
@@ -5,11 +5,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class bind between roommateId to the house drawable ,
+ * We didn't implement the import / upload images to the firebase.
+ */
 public final class Data {
     private static Map<String, Integer> uidToDrawable = new HashMap<String, Integer>();
     private static ArrayList<Integer> apartmentImages;
     private static ArrayList<String> roommateSearchersUids;
 
+    /**
+     * Initialize the houses images
+     */
     private static void initDrawablesImg() {
         apartmentImages = new ArrayList<>(Arrays.asList(R.drawable.house1,
                 R.drawable.house2,
@@ -18,22 +25,39 @@ public final class Data {
                 R.drawable.house6, R.drawable.house7));
     }
 
+    /**
+     * Initialize roommate searcher Id's (keys from firebase).
+     */
     private static void initRSUids() {
         roommateSearchersUids = new ArrayList<>(Arrays.asList("-Ly_2md3br6ex6H_4iGF", "-Ly_8mKAe1c_aPjqdese", "-Ly_Av7DnafQgO9fA3DL",
                 "-Ly_B0kHTiAnAmuwK5ol", "-Ly_F9eAenahqGxLChRo", "-LymPFoE5xAfXLBnHWl4", "-LymRsVQOemjzaCqRISh"));
     }
 
-    public static void initD() { //todo need to call this
+    /**
+     * Initialize the uidToDrawable hashmap(bind between id to drawable).
+     */
+    public static void initD() {
         initDrawablesImg();
         initRSUids();
         for (int i = 0; i < roommateSearchersUids.size(); i++) {
             uidToDrawable.put(roommateSearchersUids.get(i), apartmentImages.get(i));
         }
     }
+
+    /**
+     * Get the house drawable according to Id string
+     * @param roommateUid - roommate Id
+     * @return - The drawable
+     */
     public static int getImageByUid(String roommateUid){
         return uidToDrawable.get(roommateUid);
     }
 
+    /**
+     * Get the Id of roommate user according to the drawable
+     * @param imgId - The drawable image Id
+     * @return - The roommate Id
+     */
     public static String getUidByImg(int imgId){
         for (String key : uidToDrawable.keySet()){
             int val = uidToDrawable.get(key);
