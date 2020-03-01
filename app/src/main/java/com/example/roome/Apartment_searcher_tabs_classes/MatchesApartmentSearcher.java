7fdb566dcc216f45348a59ce5663d10efd4e1258
edenbrillant <39674183+edenbrillant@users.noreply.github.com>
2020-03-01 14:08:05 +0200

    added documentation

--- app/src/main/java/com/example/roome/Apartment_searcher_tabs_classes/MatchesApartmentSearcher.java
+++ app/src/main/java/com/example/roome/Apartment_searcher_tabs_classes/MatchesApartmentSearcher.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,10 +19,15 @@ import com.example.roome.RecyclerAdapter;
 
 import java.util.ArrayList;
 
+/**
+ * This class implements the Matches fragment. It shows the Apartments the user liked and was liked
+ * back (there was a match). Info such as Rent and location is shown below the picture of the matched
+ * apartment. In addition the phone number is shown - this enables privacy since the phone number
+ * is shown ony after there is a match (both sides approved one another).
+ */
 public class MatchesApartmentSearcher extends Fragment {
 
     private RecyclerView recyclerView;
-    //todo:refactor in the end and call it matches
     private int[] images;
     private RecyclerAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -31,26 +35,32 @@ public class MatchesApartmentSearcher extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_matches_apartment_searcher);
     }
 
+    /**
+     * Inflate the layout for this fragment
+     * @param inflater LayoutInflater
+     * @param container ViewGroup
+     * @param savedInstanceState Bundle
+     * @return view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.activity_matches_apartment_searcher, container, false);
     }
 
+    /**
+     * Initializes the Matches fragment by presenting in the page the matched apartments - takes the
+     * data from the firebase
+     * @param savedInstanceState Bundle
+     */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        ArrayList<String> matchedUids = FirebaseMediate.getAptPrefList(ChoosingActivity.YES_TO_HOUSE, MyPreferences.getUserUid(getContext()));
+        ArrayList<String> matchedUids = FirebaseMediate.getAptPrefList(ChoosingActivity.YES_TO_HOUSE,
+                MyPreferences.getUserUid(getContext()));
         initMatchedImages(matchedUids);
         if (matchedUids.size() != 0){
-//            RecyclerView rv = getView().findViewById(R.id.rv_matches_as);
-//
-//            rv.setBackground(ContextCompat.getDrawable(getContext(),
-//                    R.drawable.you_have_no_matches_yet));
             ImageView noMatches = getView().findViewById(R.id.iv_no_matches);
             noMatches.setVisibility(View.INVISIBLE);
             recyclerView = getView().findViewById(R.id.rv_matches_as);
@@ -63,11 +73,15 @@ public class MatchesApartmentSearcher extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    /**
+     * Initializes the image array of matched apartments
+     * @param matchedUids an array containing the UIDs of roommate searcher users that are matched
+     * to this user
+     */
     private void initMatchedImages(ArrayList<String> matchedUids) {
         images = new int[matchedUids.size()];
         for (int i=0;i<matchedUids.size();i++){
             images[i]= Data.getImageByUid(matchedUids.get(i));
         }
     }
-
 }
